1651ec02c1d719b72f2e3a8a1e70f1441a7d4420
alpha.sen <alpha.sen@outlook.com>
2019-05-12 10:02:39 +0800

    alter directory structure

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+var zTreeObj;
+var zTreeSeting = {
+    async: {
+        enable: true
+    },
+    callback: {
+        onClick: zTreeOnClick
+    }
+};
+function zTreeOnClick(e, treeId, treeNode, clickFlag) {
+    $('#preview').attr('src', '/' + treeNode.path);
+}
+function getRouteList() {
+    $.get('/codelab-api/filelist', function(json) {
+        zTreeObj = $.fn.zTree.init($('#treeDemo'), zTreeSeting, json);
+        zTreeObj.expandAll(true);
+    });
+}
+$(document).ready(function() {
+    getRouteList();
+});
