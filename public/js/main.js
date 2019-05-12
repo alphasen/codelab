3077999eb6a14071319cf20da10dd3b30f5efab9
alpha.sen <alpha.sen@outlook.com>
2019-05-12 23:51:26 +0800

    click dir list dir tree

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-var zTreeObj;
+var zTreeObj, folderTree;
 var zTreeSeting = {
     async: {
         enable: true
@@ -8,14 +8,53 @@ var zTreeSeting = {
     }
 };
 function zTreeOnClick(e, treeId, treeNode, clickFlag) {
-    $('#preview').attr('src', '/codelab/truelink/' + treeNode.path);
+    if (treeNode.type === 'folder') {
+        if (folderTree) {
+            // clear nodes start
+            var nodes = folderTree.getNodes();
+            for (var i = nodes.length - 1; i >= 0; i--) {
+                folderTree.removeNode(nodes[i]);
+            }
+            // clear nodes end
+
+            folderTree.addNodes(null, treeNode.children);
+        }
+        treeNode.path
+            ? history.pushState(
+                  null,
+                  'codelab|' + treeNode.name,
+                  '/' + treeNode.path
+              )
+            : '';
+        $('#preview').css('display', 'none');
+        $('#folder_container').css('display', 'block');
+    } else {
+        $('#preview').attr('src', '/codelab/truelink/' + treeNode.path);
+        treeNode.path
+            ? history.pushState(
+                  null,
+                  'codelab|' + treeNode.name,
+                  '/' + treeNode.path
+              )
+            : '';
+        $('#preview').css('display', 'block');
+        $('#folder_container').css('display', 'none');
+    }
 }
-function getRouteList() {
+function initTree() {
     $.get('/codelab-api/filelist', function(json) {
         zTreeObj = $.fn.zTree.init($('#treeDemo'), zTreeSeting, json);
         zTreeObj.expandAll(true);
+        if (!isDirRequest) {
+            folderTree = $.fn.zTree.init($('#folder_list'), zTreeSeting, json);
+            folderTree.expandAll(true);
+        }
     });
+    if (isDirRequest) {
+        var dir = location.pathname;
+        $.get('/codelab-api/filelist?dir=' + dir, function(json) {
+            folderTree = $.fn.zTree.init($('#folder_list'), zTreeSeting, json);
+            folderTree.expandAll(true);
+        });
+    }
 }
-$(document).ready(function() {
-    getRouteList();
-});
