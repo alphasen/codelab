637ed8b35ff6129909bd65d521bb2109bf2cd9d9
alpha.sen <alpha.sen@outlook.com>
2019-05-14 21:56:09 +0800

    debug js code and edit html code is ok

--- public/js/main.js
+++ public/js/main.js
@@ -27,7 +27,7 @@ function zTreeOnClick(e, treeId, treeNode, clickFlag) {
             : '';
         showContainers(['folder_container'])
     } else {
-        if (treeNode.path.endsWith('html') || treeNode.path.endsWith('htm')) {// 如果是可以预览的
+        if (treeNode.path.endsWith('.html') || treeNode.path.endsWith('.htm')) {// 如果是可以预览的
             $('#preview').attr('src', '/codelab/truelink/' + treeNode.path);
             treeNode.path
                 ? history.pushState(
@@ -36,10 +36,17 @@ function zTreeOnClick(e, treeId, treeNode, clickFlag) {
                     '/' + treeNode.path
                 )
                 : '';
-            showContainers(['preview'])
-        } else {
-            // editor.setValue("");    //给代码框赋值
-            // editor.getValue();    //获取代码框的值
+            $('#edit_btn').attr('data-nodes',JSON.stringify(treeNode))
+            $('#edit_btn').on('click',function () {
+                var nodeAttr=$('#edit_btn').attr('data-nodes')
+                var node=JSON.parse(nodeAttr||'')
+                $.get('/codelab/truelink/' + node.path, function (res) {
+                    editor.setValue(res);
+                    showContainers(['editor', 'save_btn'])
+                })
+            })
+            showContainers(['preview','edit_btn'])
+        }else {
             $.get('/codelab/truelink/' + treeNode.path, function (res) {
                 editor.setValue(res);
             })
@@ -54,25 +61,61 @@ function zTreeOnClick(e, treeId, treeNode, clickFlag) {
                         content: editor.getValue()
                     },
                     success: function (data) { // TODO 回调函数未执行
-                        console.log(data)
                         $('#save_btn').text('Save')
                         if (data.code !== 0) {
                             $('<span>error</span>').appendTo($('#save_btn'))
                         }
                     },
+                    error:function (error) {
+                        console.log('error :', error);
+                    }
                 });
             })
-            showContainers(['editor', 'save_btn'])
+            if(treeNode.path.endsWith('.js')){ // js file can debug
+                showContainers(['editor','debug_btn','save_btn'])
+                $('#debug_btn').on('click', function (e) {
+                    e.preventDefault()
+                    $('#debug_btn').text('...')
+                    $.ajax({
+                        type: "POST",
+                        url: '/codelab-api/debugfile',
+                        data: {
+                            filepath: treeNode.path,
+                            content: editor.getValue()
+                        },
+                        success: function (data) { // TODO 回调函数未执行
+                            $('#debug_btn').text('Debug')
+                            $('#sidebar_tree').css('display','none')
+                            $('#editor_preview').attr('src', '/codelab-statics/debugjs/' + treeNode.path);
+                            showContainers(['editor','debug_btn','save_btn','editor_preview','stop_btn'])
+                            $('#stop_btn').on('click',function () {
+                                showContainers(['editor','debug_btn','save_btn','sidebar_tree'])
+                            })
+                        },
+                        error:function (err) {
+                            console.log(err)
+                        }
+                    });
+                })
+            }else{
+                showContainers(['editor', 'save_btn'])
+            }
         }
     }
 }
 
 function showContainers(containers) {
     !containers ? containers = [] : ''
-    $('#preview').css('display', containers.includes('preview') ? 'block' : 'none');
-    $('#folder_container').css('display', containers.includes('folder_container') ? 'block' : 'none');
-    $('#editor').css('display', containers.includes('editor') ? 'block' : 'none');
-    $('#save_btn').css('display', containers.includes('save_btn') ? 'block' : 'none');
+    $('#preview').css('display', containers.includes('preview') ? 'unset' : 'none');
+    $('#folder_container').css('display', containers.includes('folder_container') ? 'unset' : 'none');
+    $('#editor').css('display', containers.includes('editor') ? 'unset' : 'none');
+    $('#save_btn').css('display', containers.includes('save_btn') ? 'unset' : 'none');
+    $('#run_btn').css('display', containers.includes('run_btn') ? 'unset' : 'none');
+    $('#debug_btn').css('display', containers.includes('debug_btn') ? 'unset' : 'none');
+    $('#edit_btn').css('display', containers.includes('edit_btn') ? 'unset' : 'none');
+    $('#stop_btn').css('display', containers.includes('stop_btn') ? 'unset' : 'none');
+    $('#editor_preview').css('display', containers.includes('editor_preview') ? 'unset' : 'none');
+    $('#sidebar_tree').css('display', containers.includes('sidebar_tree') ? 'unset' : 'none');
 }
 
 function initTree() {
